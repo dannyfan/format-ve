f3fdb495835eeadc03be8b6803bdf7fd2271e6f4
Danny Fan <dannyfan@live.com>
2020-06-10 16:21:06 -0400

    feat: add colors and fix checking extensions

--- cli.py
+++ cli.py
@@ -6,6 +6,8 @@ CLI used to organize files for video editing.
 import os
 import click
 
+from colorama import init, Fore 
+
 @click.command()
 @click.option("--audio", default="Audio", help="Directory name for mp3 files.",
               show_default=True)
@@ -15,6 +17,7 @@ import click
               show_default=True)
 def main(audio, video, image):
     """ Program to sort various files into respective folders for video editing use."""
+    init(autoreset=True)
     directory_list = [audio, video, image]
     create_directory(directory_list)
     sort_files(directory_list)
@@ -25,48 +28,49 @@ def file_to_which_directory(file_extension, directory_list):
     video = directory_list[1]
     image = directory_list[2]
     extensions = {
-        "mp3" : audio,
-        "mov" : video,
-        "mp4" : video,
-        "png" : image,
-        "jpg" : image,
+        ".mp3" : audio,
+        ".mov" : video,
+        ".mp4" : video,
+        ".png" : image,
+        ".jpg" : image,
     }
-    return extensions.get(file_extension, None)
+    return extensions.get(file_extension)
 
 def sort_files(directory_list):
     """ Sort files in root directory and move to appropiate directory."""
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    if len(files) == 0:
+        click.echo(Fore.YELLOW + f"No files found in directory!")
+
     for fil in files:
-        name, extension = os.path.splitext(fil)
-        directory = file_to_which_directory(extension, directory_list)
-        print(name)
-        print(extension)
+        NAME, EXTENSION = os.path.splitext(fil)
+        DIRECTORY = file_to_which_directory(EXTENSION, directory_list)
         # Extension doesn't exist so it would not be moved
-        if directory is None:
-            pass
+        if DIRECTORY is None:
+            continue
 
         try:
-            os.rename(fil, f"{directory}/{fil}")
-            click.echo(f"Moving {name} into {directory} directory...")
+            os.rename(fil, f"{DIRECTORY}/{fil}")
+            click.echo(Fore.LIGHTCYAN_EX + f"Moving {NAME} into {DIRECTORY} directory...")
         except OSError:
-            click.echo(f"Error moving {name} into {fil} {directory} directory...")
+            click.echo(Fore.RED + f"Error moving into {NAME} {DIRECTORY} directory.")
         else:
-            click.echo(f"{name} successfully moved into {directory}!")
+            click.echo(Fore.GREEN + f"DONE! Successfully moved {NAME} into {DIRECTORY}.")
 
 def create_directory(directory_list):
     """ Create directories with name provided. """
     for directory in directory_list:
         exists = os.path.isdir(directory)
         if exists:
-            click.echo(f"Skipping creation of {directory} directory. Directory already exists.")
+            click.echo(Fore.YELLOW + f"Skipping creation of {directory} directory. Directory already exists.")
             continue
         try:
             os.mkdir(directory)
-            click.echo(f"Creating {directory} directory...")
+            click.echo(Fore.LIGHTCYAN_EX + f"Creating {directory} directory...")
         except OSError:
-            click.echo(f"Error creating {directory} directory...")
+            click.echo(Fore.RED + f"Error creating {directory} directory...")
         else:
-            click.echo(f"{directory} directory successfully created!")
+            click.echo(Fore.GREEN + f"DONE! {directory} directory successfully created.")
 
 if __name__ == '__main__':
     # pylint: disable=no-value-for-parameter
